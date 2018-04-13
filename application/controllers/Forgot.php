7cc3bf08a040b705268b2653deb237e085192f97
Muh Husein <hiz.husein@gmail.com>
2018-04-13 13:01:06 +0700

    commit
    reset, forgot

--- application/controllers/Forgot.php
+++ application/controllers/Forgot.php
@@ -1,19 +1,20 @@
-
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-class Welcome extends CI_Controller {
+class Forgot extends CI_Controller {
 
-	function __construct() {
-		parent::__construct();
+    function __construct() {
+        parent::__construct();
+        $this->load->model('User_model', 'user_model', TRUE);
+        $this->load->library('form_validation');
 
-	}  
+    }
 
 
-	public function index()
-	{
-		$this->load->view('forgot.php');
-	}
+    public function index()
+    {
+        $this->load->view('pages/forgot');
+    }
 }
