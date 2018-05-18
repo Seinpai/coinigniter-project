428d8539fbb81f6f062a5f403e15478d9ba64b52
Muh Husein <hiz.husein@gmail.com>
2018-05-19 01:39:37 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/Seinpai/coinigniter-project""
    This reverts commit 1f7c1caf06aa03db93645ba2341f1d94f5089de3.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -98,6 +98,7 @@
             <div class="row">
                 <div class="col s12 m12">
                 <div class="ui three stackable cards">
+<<<<<<< HEAD
                   <?php foreach ($data as $key => $value) { 
                   $datestring = '%Y-%m-%d';
                   $time = time();
@@ -119,28 +120,184 @@
                   <div class="content">
                     <a class="header" href="campaign/'.$value['id'].'">
                     '.$value['campaign_name'].' 
+=======
+
+                  <div class="card">
+                    <div class="content"><span>
+                      Elliot
+                      <i class="tiny material-icons">check_circle</i>
+                    </span></div>
+                    <a class="image" href="#">
+                      <img src="<?php echo base_url()?>public/img/background4.jpg">
+>>>>>>> 4e480fc40d6cdc1d440854e81e882aa81c86ed85
                     </a>
-                  </div>
-                  <div class="extra content">
-                    <span class="right floated">
-                      '.$convert.' Days left
-                    </span>
-                    <span>
-                      <i class="user icon"></i>
-                      '.$value['campaign_current_fund'].' Bitcoins Funded
-                    </span>
-                  </div>
-                  <div class="ui bottom attached">
-                    <div class="ui indicating progress" data-value='.$value['campaign_current_fund'].' data-total='.$value['campaign_target_fund'].'>
-                      <div class="bar">
-                        <div class="progress"></div>
+                    <div class="content">
+                      <a class="header" href="#">Campaign Title Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+                    </div>
+                    <div class="extra content">
+                      <span class="right floated">
+                        23 Days left
+                      </span>
+                      <span>
+                        <i class="user icon"></i>
+                        0.0009 Bitcoins Funded
+                      </span>
+                    </div>
+                    <div class="ui bottom attached">
+                      <div class="ui indicating progress" data-value="14" data-total="20">
+                        <div class="bar">
+                          <div class="progress"></div>
+                        </div>
+                        <div class="label">Uploading Files</div>
                       </div>
-                      <div class="label">Uploading Files</div>
                     </div>
                   </div>
-                </div>';
 
-                }?>
+                  <div class="card">
+                    <div class="content"><span>
+                      Elliot
+                      <i class="tiny material-icons">check_circle</i>
+                    </span></div>
+                    <a class="image" href="#">
+                      <img src="<?php echo base_url()?>public/img/background4.jpg">
+                    </a>
+                    <div class="content">
+                      <a class="header" href="#">Campaign Title Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+                    </div>
+                    <div class="extra content">
+                      <span class="right floated">
+                        23 Days left
+                      </span>
+                      <span>
+                        <i class="user icon"></i>
+                        0.0009 Bitcoins Funded
+                      </span>
+                    </div>
+                    <div class="ui bottom attached">
+                      <div class="ui indicating progress" data-value="10" data-total="20">
+                        <div class="bar">
+                          <div class="progress"></div>
+                        </div>
+                        <div class="label">Uploading Files</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card">
+                    <div class="content"><span>
+                      Elliot
+                      <i class="tiny material-icons">check_circle</i>
+                    </span></div>
+                    <a class="image" href="#">
+                      <img src="<?php echo base_url()?>public/img/background4.jpg">
+                    </a>
+                    <div class="content">
+                      <a class="header" href="#">Campaign Title Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+                    </div>
+                    <div class="extra content">
+                      <span class="right floated">
+                        23 Days left
+                      </span>
+                      <span>
+                        <i class="user icon"></i>
+                        0.0009 Bitcoins Funded
+                      </span>
+                    </div>
+                    <div class="ui bottom attached">
+                      <div class="ui indicating progress" data-value="18" data-total="20">
+                        <div class="bar"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div class="row">
+                <div class="col s12 m12">
+                <div class="ui three stackable cards">
+
+                  <div class="card">
+                    <div class="content"><span>
+                      Elliot
+                      <i class="tiny material-icons">check_circle</i>
+                    </span></div>
+                    <a class="image" href="#">
+                      <img src="<?php echo base_url()?>public/img/background4.jpg">
+                    </a>
+                    <div class="content">
+                      <a class="header" href="#">Campaign Title Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+                    </div>
+                    <div class="extra content">
+                      <span class="right floated">
+                        23 Days left
+                      </span>
+                      <span>
+                        <i class="user icon"></i>
+                        0.0009 Bitcoins Funded
+                      </span>
+                    </div>
+                    <div class="ui bottom attached">
+                      <div class="ui indicating progress" data-value="18" data-total="20" id="example5">
+                        <div class="bar"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card">
+                    <div class="content"><span>
+                      Elliot
+                      <i class="tiny material-icons">check_circle</i>
+                    </span></div>
+                    <a class="image" href="#">
+                      <img src="<?php echo base_url()?>public/img/background4.jpg">
+                    </a>
+                    <div class="content">
+                      <a class="header" href="#">Campaign Title Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+                    </div>
+                    <div class="extra content">
+                      <span class="right floated">
+                        23 Days left
+                      </span>
+                      <span>
+                        <i class="user icon"></i>
+                        0.0009 Bitcoins Funded
+                      </span>
+                    </div>
+                    <div class="ui bottom attached">
+                      <div class="ui indicating progress" data-value="18" data-total="20" id="example5">
+                        <div class="bar"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card">
+                    <div class="content"><span>
+                      Elliot
+                      <i class="tiny material-icons">check_circle</i>
+                    </span></div>
+                    <a class="image" href="#">
+                      <img src="<?php echo base_url()?>public/img/background4.jpg">
+                    </a>
+                    <div class="content">
+                      <a class="header" href="#">Campaign Title Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
+                    </div>
+                    <div class="extra content">
+                      <span class="right floated">
+                        23 Days left
+                      </span>
+                      <span>
+                        <i class="user icon"></i>
+                        0.0009 Bitcoins Funded
+                      </span>
+                    </div>
+                    <div class="ui bottom attached">
+                      <div class="ui indicating progress" data-value="18" data-total="20" id="example5">
+                        <div class="bar"></div>
+                      </div>
+                    </div>
+                  </div>
+
                 </div>
               </div>
             </div>
